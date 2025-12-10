b5acf383af07f8a26edaf84ca2f603115e27320d
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-12-10 10:12:57 +0600

    Update brute.py

--- brute.py
+++ brute.py
@@ -7,8 +7,7 @@ post_url='https://www.facebook.com/login.php'
 headers = {
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
-import mechanize
-import urllib2
+
 browser = mechanize.Browser()
 browser.addheaders = [('User-Agent',headers['User-Agent'])]
 browser.set_handle_robots(False)
